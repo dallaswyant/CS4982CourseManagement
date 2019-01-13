4505df24416c0ed1d4a7205082060b6d880b5c89
corey thornton <cthornt8@my.westga.edu>
2019-01-13 13:46:06 -0500

    Began implementing model classes

---
+++ CourseManagement/CourseManagement/App_Code/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace CourseManagement.App_Code
+{
+    public class Course
+    {
+        public string description { get; private set; }
+    }
+}
