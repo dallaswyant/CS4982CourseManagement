164991ebd70636c2007c4275b2bc604b7323411a
corey thornton <cthornt8@my.westga.edu>
2019-01-13 15:45:07 -0500

    Added Basic Model Classes

---
+++ CourseManagement/CourseManagement/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CourseManagement.App_Code
+{
+    public class Student
+    {
+        public string name { get; private set; }
+        public string Email { get; private set; }
+        public CourseCollection activeCourses { get; private set; }
+        public DegreeProgram Program { get; private set; }
+    }
+}
