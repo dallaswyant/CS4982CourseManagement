c9fbc6b726d106b84dbbb485ce527af304d0471d
Dallas Wyant <dwyant1@my.westga.edu>
2019-01-15 14:59:48 -0500

    adding basic model classes back into project

---
+++ CourseManagement/CourseManagement/Models/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CourseManagement.App_Code
+{
+    public class Department
+    {
+        public Teacher Chair { get; private set; }
+        public CourseCollection DeptCourses { get; private set; }
+        public string DeptName { get; private set; }
+        public List<Teacher> teachers { get; private set; }
+
+        public Department(Teacher chair, CourseCollection deptCourses, string deptName, List<Teacher> teachers)
+        {
+            this.Chair = chair;
+            this.DeptCourses = deptCourses;
+            this.DeptName = deptName;
+            this.teachers = teachers;
+        }
+    }
+}
