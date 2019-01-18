1de57e0a54794227e855e3533baec4115599e281
Corey Thornton <cthornt8@my.westga.edu>
2019-01-18 13:23:48 -0500

    added user class

---
+++ CourseManagement/CourseManagement/App_Code/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CourseManagement.App_Code
+{
+    public class User
+    {
+        public string UserID { get; private set; }
+        public string password { get; private set; }
+        public string role { get; private set; }
+
+        public User(string userID, string password, string role)
+        {
+            UserID = userID;
+            this.password = password;
+            this.role = role;
+        }
+
+        public User()
+        {
+        }
+    }
+}
