10fd740067942ab4c1d5514c49954ff9e3f99d3e
Dallas Wyant <dwyant1@my.westga.edu>
2019-01-19 22:36:37 -0500

    added mySQL references and dbConnection class

---
+++ CourseManagement/CourseManagement/DAL/DbConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace CourseManagement.DAL
+{
+    public class DbConnection
+    {
+        /// <summary>
+        /// Gets the connection.
+        /// </summary>
+        /// <returns> the connection for the db</returns>
+        public static MySqlConnection GetConnection()
+        {
+
+            string conStr = "server=160.10.25.16; port=3306; uid=cs4982s19d;" +
+                            "pwd=H0KQ5qCgSXKDNdp5; database=cs4982s19d;";
+
+            MySqlConnection conn = new MySqlConnection(conStr);
+            return conn;
+        }
+    }
+}
