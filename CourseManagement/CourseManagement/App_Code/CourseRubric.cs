5bbc0438e1f4bc4a473e1deb39ad3fd126267ca1
Corey Thornton <cthornt8@my.westga.edu>
2019-01-20 13:05:09 -0500

    added Rubric class

---
+++ CourseManagement/CourseManagement/App_Code/CourseRubric.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CourseManagement.App_Code
+{
+    public class CourseRubric
+    {
+        public Dictionary<string,int> GradeTypeWithWeights { get;private set; }
+
+        public CourseRubric(Dictionary<string, int> gradeTypeWithWeights)
+        {
+            GradeTypeWithWeights = gradeTypeWithWeights;
+        }
+
+
+    }
+}
