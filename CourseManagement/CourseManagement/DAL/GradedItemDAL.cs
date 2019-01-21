d4dcf63ce7100f764aa21cd9594eb2c15057be4b
Dallas Wyant <dwyant1@my.westga.edu>
2019-01-21 18:34:28 -0500

    added gradedItemDAL method getGradedItemsByCRNAndGradeName

--- CourseManagement/CourseManagement/DAL/GradedItemDAL.cs
+++ CourseManagement/CourseManagement/DAL/GradedItemDAL.cs
@@ -9,6 +9,8 @@ namespace CourseManagement.DAL
     {
         #region Methods
 
+        
+
         public List<GradedItem> GetGradedItemsByCRN(int CRNCheck)
         {
             MySqlConnection conn = DbConnection.GetConnection();
@@ -156,35 +158,46 @@ namespace CourseManagement.DAL
             }
         }
 
-        public void InsertNewGradedItem(GradedItem newItem)
+        public void InsertNewGradedItemByCRN(GradedItem newItem, int CRN)
         {
-            MySqlConnection conn = DbConnection.GetConnection();
-            using (conn)
-            {
-                conn.Open();
-                var selectQuery =
-                    "INSERT INTO grade_items(student_id, grade_total_points, grade_earned_points, grade_type, grade_name, grade_feedback) VALUES (@studentUID,@grade_total,@grade_points,@grade_type,@grade_name,@grade_feedback)";
-                using (MySqlCommand cmd = new MySqlCommand(selectQuery, conn))
+            StudentDAL studentGetter = new StudentDAL();
+            List<Student> students = studentGetter.GetStudentsByCRN(CRN);
+            
+                
+                MySqlConnection conn = DbConnection.GetConnection();
+
+                using (conn)
                 {
-                    cmd.Parameters.AddWithValue("@studentUID", newItem.Student.StudentUID);
-                    cmd.Parameters.AddWithValue("@grade_total",newItem.Grade);
-                    cmd.Parameters.AddWithValue("@grade_points",newItem.PossiblePoints);
-                    cmd.Parameters.AddWithValue("@grade_type", newItem.GradeType);
-                    cmd.Parameters.AddWithValue("@grade_name", newItem.Name);
-                    cmd.Parameters.AddWithValue("@grade_feedback", newItem.Feedback);
-                    cmd.ExecuteNonQuery();
+                    conn.Open();
+                    foreach (var t in students)
+                    {
+                        GradedItem grade = new GradedItem(newItem.Name, t, 0.0, null, newItem.PossiblePoints, null, 0);
+                        var selectQuery =
+                            "INSERT INTO grade_items(student_id, grade_total_points, grade_earned_points, grade_type, grade_name, grade_feedback) VALUES (@studentUID,@grade_total,@grade_points,@grade_type,@grade_name,@grade_feedback)";
+                        using (MySqlCommand cmd = new MySqlCommand(selectQuery, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@studentUID", newItem.Student.StudentUID);
+                            cmd.Parameters.AddWithValue("@grade_total", newItem.Grade);
+                            cmd.Parameters.AddWithValue("@grade_points", newItem.PossiblePoints);
+                            cmd.Parameters.AddWithValue("@grade_type", newItem.GradeType);
+                            cmd.Parameters.AddWithValue("@grade_name", newItem.Name);
+                            cmd.Parameters.AddWithValue("@grade_feedback", newItem.Feedback);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    conn.Close();
                 }
-                conn.Close();
-            }
+            
         }
 
-        public void UpdateGradeItem(GradedItem newItem)
+        public void UpdateGradeItem(GradedItem newItem,int CRN)
         {
 
             MySqlConnection conn = DbConnection.GetConnection();
-            string sql = "UPDATE grade_items SET grade_total_points=@grade_total, grade_earned_points=@grade_points, grade_type=@grade_type, grade_name=@grade_name, grade_feedback=@grade_feedback WHERE grade_item_id=@grade_item_id";
+            string sql = "UPDATE grade_items SET grade_total_points=@grade_total, grade_earned_points=@grade_points, grade_type=@grade_type, grade_name=@grade_name, grade_feedback=@grade_feedback WHERE student_uid = @studentUID";
             conn.Open();
             MySqlCommand cmd = new MySqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@studentUID", newItem.Student.StudentUID);
             cmd.Parameters.AddWithValue("@grade_total", newItem.Grade);
             cmd.Parameters.AddWithValue("@grade_points", newItem.PossiblePoints);
             cmd.Parameters.AddWithValue("@grade_type", newItem.GradeType);
@@ -194,6 +207,64 @@ namespace CourseManagement.DAL
             cmd.ExecuteNonQuery();
             conn.Close();
 
+        }
+
+        public List<GradedItem> GetGradedItemsByCRNAndGradeName(int CRNCheck, string gradeName)
+        {
+            MySqlConnection conn = DbConnection.GetConnection();
+            var coursesTaught = new CourseCollection();
+            var grades = new List<GradedItem>();
+            using (conn)
+            {
+                conn.Open();
+                var selectQuery =
+                    "SELECT * FROM grade_items,grade_belongs_to_courses WHERE grade_items.grade_item_id = grade_belongs_to_courses.grade_item_id AND grade_belongs_to_courses.courses_CRN = @CRNCheck AND grade_name = @grade_name";
+                var studentGetter = new StudentDAL();
+                using (MySqlCommand cmd = new MySqlCommand(selectQuery, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CRNCheck", CRNCheck);
+                    cmd.Parameters.AddWithValue("@grade_name", gradeName);
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        int studentIdOrdinal = reader.GetOrdinal("student_uid");
+                        int totalPointsOrdinal = reader.GetOrdinal("grade_total_points");
+                        int gradeEarnedOrdinal = reader.GetOrdinal("grade_earned_points");
+                        int gradeTypeOrdinal = reader.GetOrdinal("grade_type");
+                        int gradeFeedbackOrdinal = reader.GetOrdinal("grade_feedback");
+                        int gradeItemIdOrdinal = reader.GetOrdinal("grade_item_id");
+
+                        while (reader.Read())
+                        {
+                            var studentUID = reader[studentIdOrdinal] == DBNull.Value
+                                ? default(string)
+                                : reader.GetString(studentIdOrdinal);
+                            var totalPoints = reader[totalPointsOrdinal] == DBNull.Value
+                                ? default(int)
+                                : reader.GetInt32(totalPointsOrdinal);
+                            var gradeEarned = reader[gradeEarnedOrdinal] == DBNull.Value
+                                ? default(int)
+                                : reader.GetInt32(gradeEarnedOrdinal);
+                            var gradeType = reader[gradeTypeOrdinal] == DBNull.Value
+                                ? default(string)
+                                : reader.GetString(gradeTypeOrdinal);
+                            var gradeFeedback = reader[gradeFeedbackOrdinal] == DBNull.Value
+                                ? default(string)
+                                : reader.GetString(gradeFeedbackOrdinal);
+                            var gradeItemId = reader[gradeItemIdOrdinal] == DBNull.Value
+                                ? default(int)
+                                : reader.GetInt32(gradeItemIdOrdinal);
+
+                            var currStudent = studentGetter.GetStudentByStudentID(studentUID);
+
+                            var currGradedItem = new GradedItem(gradeName, currStudent, gradeEarned, gradeFeedback, totalPoints,
+                                gradeType, gradeItemId);
+                            grades.Add(currGradedItem);
+                        }
+
+                        return grades;
+                    }
+                }
+            }
         }
 
         #endregion
