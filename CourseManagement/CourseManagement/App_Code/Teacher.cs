3d6b18e1727cd1a4467fab06c7a79f09a378ffa7
Corey Thornton <cthornt8@my.westga.edu>
2019-02-04 21:06:39 -0500

    added documentation for model classes

--- CourseManagement/CourseManagement/App_Code/Teacher.cs
+++ CourseManagement/CourseManagement/App_Code/Teacher.cs
@@ -5,19 +5,52 @@ namespace CourseManagement.App_Code
     public class Teacher
     {
         #region Properties
+        /// <summary>
+        /// gets the teacher User ID
+        /// </summary>
         public string TeacherUID { get; }
+        /// <summary>
+        /// gets the location
+        /// </summary>
         public string Location { get; }
+        /// <summary>
+        /// gets the name
+        /// </summary>
         public string Name { get; }
+        /// <summary>
+        /// gets the email
+        /// </summary>
         public string Email { get; }
+        /// <summary>
+        /// gets if the email is public
+        /// </summary>
         public bool IsEmailPublic { get; }
+        /// <summary>
+        /// gets the phonenumber
+        /// </summary>
         public string PhoneNumber { get; }
+        /// <summary>
+        /// gets the primary department
+        /// </summary>
         public Department PrimaryDepartment { get; }
+        /// <summary>
+        /// gets the course taught
+        /// </summary>
         public List<Course> CoursesTaught { get; }
 
         #endregion
 
         #region Constructors
-
+       /// <summary>
+       /// constructor for a teacher
+       /// </summary>
+       /// <param name="location">the office location</param>
+       /// <param name="name">the teachers name</param>
+       /// <param name="email">the teachers email</param>
+       /// <param name="isEmailPublic">if the email is public</param>
+       /// <param name="phoneNumber">teachers phone number</param>
+       /// <param name="coursesTaught">the course they teach</param>
+       /// <param name="teacherUID">the teachers user id</param>
         public Teacher(string location, string name, string email, bool isEmailPublic, string phoneNumber,
             List<Course> coursesTaught, string teacherUID)
         {
