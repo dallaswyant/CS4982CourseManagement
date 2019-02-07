4fe7ad4a10996d82f017cb0870e1660cd9fc6525
Dallas Wyant <dwyant1@my.westga.edu>
2019-02-06 23:40:36 -0500

    added unit tests for encrypter

--- CourseManagement/CourseManagementUnitTests/Utilities/EncrypterTest.cs
+++ CourseManagement/CourseManagementUnitTests/Utilities/EncrypterTest.cs
@@ -4,20 +4,39 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CourseManagementUnitTests.Utilities
 {
+    /// <summary>
+    /// This Class tests the encryption Utility class for encryption and decryption
+    /// </summary>
     [TestClass]
     public class EncrypterTest
     {
+        /// <summary>
+        /// Tests the encryption against one password.
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void TestEncryptionAgainstOne()
         {
-            String thing = Encrypter.Encrypt("password", "raspberryberet");
-            String otherthing = Encrypter.Encrypt("password", "raspberryberet");
-            String originalthing = Encrypter.Decrypt(thing, "raspberryberet");
-            String testthing =
-                Encrypter.Decrypt(
-                    "fLibpTLVTaojqqqc4HbeTBhd7gOf+qS4h+y7/45IP3YGWuT2M7mdx7iNdnAYdpqI9VB0I6J0UlSPgfKSILh+SQKzUfJaiMIm+gM0GJtxJEiH5GIuhRg5Rf6iCbRP/Ha8",
-                    "raspberryberet");
-            Assert.AreEqual("password", originalthing);
+            String password = Encrypter.Encrypt("password", "raspberryberet");
+            String originalpassword = Encrypter.Decrypt(password, "raspberryberet");
+            Assert.AreEqual("password", originalpassword);
+        }
+
+        /// <summary>
+        /// Tests the encryption against three of the same passwords,
+        /// which are encrypted to three different strings.
+        /// </summary>
+        [TestMethod]
+        public void TestEncryptionAgainstThree()
+        {
+            String password = Encrypter.Encrypt("password", "raspberryberet");
+            String originalpassword = Encrypter.Decrypt(password, "raspberryberet");
+            Assert.AreEqual("password",originalpassword);
+            String password1 = Encrypter.Encrypt("password", "raspberryberet");
+            String originalpassword1 = Encrypter.Decrypt(password1, "raspberryberet");
+            Assert.AreEqual("password",originalpassword1);
+            String password2 = Encrypter.Encrypt("password", "raspberryberet");
+            String originalpassword2 = Encrypter.Decrypt(password2, "raspberryberet");
+            Assert.AreEqual("password", originalpassword2);
         }
     }
 }
