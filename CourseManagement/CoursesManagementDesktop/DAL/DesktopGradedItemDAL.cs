2e65dd177f522e27cf37d141c84e6f23711c8f54
Corey Thornton <cthornt8@my.westga.edu>
2019-02-22 12:34:18 -0500

    Fix view grade functionality for desktop

--- CourseManagement/CoursesManagementDesktop/DAL/DesktopGradedItemDAL.cs
+++ CourseManagement/CoursesManagementDesktop/DAL/DesktopGradedItemDAL.cs
@@ -22,21 +22,21 @@ namespace CoursesManagementDesktop.DAL
             using (conn)
             {
                 conn.Open();
-                //var selectQuery =     "Select Concat(fname," ", lname) as name, student_grade_items.grade_earned_points, grade_defs.grade_total_points,grade_type,student_grade_items.grade_feedback FROM personal_info,student_grade_items,grade_defs,students WHERE personal_info.uid = students.uid AND student_grade_items.student_uid = students.uid AND grade_defs.grade_def_id = student_grade_items.grade_def_id AND grade_defs.course_CRN = @CRNCheck";
-                //using (MySqlCommand cmd = new MySqlCommand(selectQuery, conn))
-                //{
-                //    cmd.Parameters.AddWithValue("@CRNCheck", CRNCheck);
-                //    cmd.Parameters.AddWithValue("@grade_name", gradeName);
+                var selectQuery = "Select Concat(fname,\" \", lname) as Student_Name, student_grade_items.grade_earned_points as Earned_Points, grade_defs.grade_total_points as Total_Points,grade_type as Grade_Type,student_grade_items.grade_feedback as Feedback FROM personal_info,student_grade_items,grade_defs,students WHERE personal_info.uid = students.uid AND student_grade_items.student_uid = students.uid AND grade_defs.grade_def_id = student_grade_items.grade_def_id AND grade_defs.course_CRN = @CRNCheck AND grade_defs.grade_name = @grade_name AND grade_defs.grade_def_id = student_grade_items.grade_def_id ";
+                using (MySqlCommand cmd = new MySqlCommand(selectQuery, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CRNCheck", CRNCheck);
+                    cmd.Parameters.AddWithValue("@grade_name", gradeName);
 
-                    
-                //    cmd.ExecuteNonQuery();
 
-                //    MySqlDataAdapter dataAdapter = new MySqlDataAdapter(cmd);
-                //    DataTable dt = new DataTable("Student Grades");
-                //    dataAdapter.Fill(dt);
-                //    grid.ItemsSource = dt.DefaultView;
-                //    conn.Close();
-                //}
+                    cmd.ExecuteNonQuery();
+
+                    MySqlDataAdapter dataAdapter = new MySqlDataAdapter(cmd);
+                    DataTable dt = new DataTable("Student Grades");
+                    dataAdapter.Fill(dt);
+                    grid.ItemsSource = dt.DefaultView;
+                    conn.Close();
+                }
             }
         }
     }
