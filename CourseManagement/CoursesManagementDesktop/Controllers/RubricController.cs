24704401439a4bd0688803662780076a8a4306b9
Corey Thornton <cthornt8@my.westga.edu>
2019-03-17 15:46:44 -0400

    Can view Rubric for course

---
+++ CourseManagement/CoursesManagementDesktop/Controllers/RubricController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CourseManagement.DAL;
+using CoursesManagementDesktop.Model;
+
+namespace CoursesManagementDesktop.Controllers
+{
+    public class RubricController
+    {
+
+        public ManageRubricPage rubricPage { get; private set; }
+        public CourseDAL courseDAL { get; private set; }
+        public SemesterDAL semesterDal { get; private set; }
+
+        public RubricController(ManageRubricPage rubricPage)
+        {
+            this.rubricPage = rubricPage;
+            this.courseDAL = new CourseDAL();
+            this.semesterDal = new SemesterDAL();
+        }
+
+
+
+
+        /// <summary>
+        ///     Populates the combo boxes.
+        /// </summary>
+        public void populateComboBoxes()
+        {
+            this.populateSemesterComboBox();
+            this.populateCourseComboBox();
+            
+        }
+
+      
+
+        private void populateCourseComboBox()
+        {
+            string semester = this.rubricPage.semesterComboBox.Text;
+            var courses = this.courseDAL.GetCoursesByTeacherAndSemester(CourseManagementTools.TeacherID,semester);
+
+            foreach (var name in courses)
+            {
+                this.rubricPage.courseComboBox.Items.Add(name.Name);
+            }
+
+            this.rubricPage.courseComboBox.SelectedIndex = 0;
+        }
+
+        private void populateSemesterComboBox()
+        {
+            int index = 0;
+            var semesters = this.semesterDal.GetAllSemesters();
+            foreach (var semester in semesters)
+            {
+                this.rubricPage.semesterComboBox.Items.Add(semester.SemesterID);
+                if (semester.StartDate < DateTime.Now & semester.EndDate > DateTime.Now)
+                {
+                    this.rubricPage.semesterComboBox.SelectedIndex = index;
+                }
+
+                index++;
+            }
+        }
+
+        /// <summary>
+        ///     Loads the data grid.
+        /// </summary>
+        //public void LoadDataGrid()
+        //{
+
+        //    var name = this.homePage.AssignmentCombo.SelectedItem == null ? "" : this.homePage.AssignmentCombo.SelectedItem.ToString();
+        //    var crn = CourseManagementTools.findCrn(this.homePage.CourseCombo.Text, this.homePage.semesterBox.Text);
+
+        //    this.desktopGradedItemDal.populateDataGrid(crn, name, this.homePage.dataGridGrades);
+        //    this.currentCrn = crn;
+
+        //}
+
+
+    }
+}
---
+++ CourseManagement/CoursesManagementDesktop/ManageRubricPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CourseManagement.DAL;
+using CourseManagement.Models;
+using CoursesManagementDesktop.Annotations;
+using CoursesManagementDesktop.Controllers;
+using CoursesManagementDesktop.Model;
+
+namespace CoursesManagementDesktop
+{
+    /// <summary>
+    /// Interaction logic for ManageRubricPage.xaml
+    /// </summary>
+    public partial class ManageRubricPage : Page,INotifyPropertyChanged 
+    {
+        public RubricController controller { get; set; }
+
+        public CourseRubricDAL rubricDAL { get; set; }
+
+        public ObservableCollection<RubricItem> rubricItems { get; set; }
+        public ManageRubricPage()
+        {
+            InitializeComponent();
+            this.controller = new RubricController(this);
+            this.controller.populateComboBoxes();
+            this.rubricDAL = new CourseRubricDAL();
+            this.rubricItems = new ObservableCollection<RubricItem>();
+            this.loadRubric();
+            DataContext = this;
+        }
+
+        private void loadRubric()
+        {
+            var crn = CourseManagementTools.findCrn(this.courseComboBox.Text,this.semesterComboBox.Text);
+           var items =  this.rubricDAL.GetCourseRubricByCRN(crn);
+            foreach (var rubricItem in items)
+            {
+                this.rubricItems.Add(rubricItem);
+            }
+
+        }
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
